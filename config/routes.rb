35d6cdd0b4a4e1b29e9117633ba70ed067830ca9
Zach Ross <zachariahrossy@gmail.com>
2020-11-17 19:21:41 +0100

    conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
+  # devise_scope :user do
+  #   get '/check_active' to: 'sessions#check_active'
+  # end
+  # Ticke pour 18/10/2020
   resources :confinements do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :show]
   end
   resources :bookings, only: [:index, :destroy]
 end
