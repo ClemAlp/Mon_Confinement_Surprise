f3b9cc3508689a437dabf57d34e605e76c7d3f95
adifsgaid <infosgaid@gmail.com>
2020-11-19 20:13:03 +0100

    clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,12 @@ puts "Cleaning database..."
 Booking.destroy_all
 Confinement.destroy_all
 User.destroy_all
-
 puts "Adding Confinement to the DB"
-
 user = User.create!(email: 'zachariahrossy@gmail.com', password: 'testing', name: 'Zach')
-conf_1 = Confinement.create!(name: 'Campagne', description: 'Un super confinement à la campagne', user: user)
 
-file = URI.open('https://static.hitek.fr/img/up_m/1830308727/topmemecoronavirus4.jpg')
-conf_1.photo.attach(io: file, filename: 'gamer_confinement.png', content_type: 'image/png')
+conf_2 = Confinement.new(name: 'Combo télétravail et école à la maison
+', description: 'Vous avez toujours rêvé de travailler en claquette chaussettes ?iser ', user: user, first_criteria:'dfsfs' )
+
+file = URI.open('https://res.cloudinary.com/drzczicvd/image/upload/v1605809523/topmemecoronavirus15_kieqmu.jpg')
+conf_2.photo.attach(io: file, filename: 'teletravail_confinement.png', content_type: 'image/png')
+conf_2.save!
